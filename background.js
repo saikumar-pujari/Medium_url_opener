5baffd63da1f7920e1b095d63b501c32bf65a13b
Justin Sebastian Thomas <160826136+Justinthomas2003@users.noreply.github.com>
2026-02-12 22:42:11 +0530

    Add comment for target=_blank instance creation
    Add comment explaining new tab creation behavior

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 chrome.action.onClicked.addListener((tab) => {
   if (!tab.url) return;
+  //this is creating a new instance as target=_blank
   const targetUrl = `https://freedium-mirror.cfd/${tab.url}`;
   chrome.tabs.create({ url: targetUrl });
 });
